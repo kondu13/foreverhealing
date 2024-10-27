d192614e6ef23bbd9df8cad43e82c4803ca710fc
Karthik Kondu <kondukarthik@gmail.com>
2024-10-28 03:37:03 +0530

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import Link from 'next/link'
-import Image from 'next/image'
 import HeroSection from '@/components/homepageComponents/HeroSection'
 import ApproachAndBenefits from '@/components/homepageComponents/ApproachAndBenefits'
 import HealingJourney from '@/components/homepageComponents/HealingJourney'
 import About from '@/components/homepageComponents/About'
-import ResearchSummary from '@/components/homepageComponents/ResearchSummary'
 import HealingJourneyCertificates from '@/components/homepageComponents/HealingJourneyCertificates'
 import SenjaTestimonials from '@/components/SenjaTestimonials'
 
